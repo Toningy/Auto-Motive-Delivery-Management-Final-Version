1ea10f0f65f790e0b19438b729a4cda29af77b6f
Toni Bao Huy Nguyen <toni.nguyen@students.uni-mannheim.de>
2025-12-04 22:09:57 +0800

    pushi

--- src/main/java/web/ReportServlet.java
+++ src/main/java/web/ReportServlet.java
@@ -10,7 +10,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.sql.Date;
 import java.util.HashMap;
 import java.util.Map;
 
